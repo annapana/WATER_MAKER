7fd4417a8a77604e8afbbd901896b3244eb3e2f0
son <annapana1995@gmail.com>
2023-01-18 14:52:08 +0700

    first commit

---
+++ 3D view/Data/js/threesixty.js
@@ -0,0 +1,27 @@
+
+threeSixty = {
+    init: function () {
+        this._vr = new AC.VR('viewer', 'images/Frame#####.jpg', [145, 1], {
+            invert: false
+            
+        });
+    },
+    didShow: function() {
+        this.init();
+    },
+    willHide: function() {
+        recycleObjectValueForKey(this, "_vr");
+    },
+    shouldCache: function() {
+        return false;
+    }
+    
+}
+
+
+if (!window.isLoaded) {
+    window.addEventListener("load", function() {
+        threeSixty.init();
+       
+    }, false);
+}
